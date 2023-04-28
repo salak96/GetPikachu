18a6a5b05aefb5e3944461f4709b599c55c4543f
lambang <sasangkakurniawan.com>
2023-04-28 16:38:53 +0700

    add

--- Assets/main.js
+++ Assets/main.js
@@ -2,46 +2,55 @@
 
 const pikachuContainer = document.getElementById("pikachu-container");
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+fetch("https://pokeapi.co/api/v2/pokemon/25")
   .then(response => response.json())
   .then(data => {
     addEventListener("click", () => {
-    document.getElementById('pikachu-container').innerHTML = '';
-    document.getElementById('pikachu-container')    
-    const pikachuImage = document.createElement("img");
-    pikachuImage.setAttribute("src", data.sprites.front_default);
-    pikachuImage.setAttribute("alt", "Pikachu");
-    pikachuImage.setAttribute("id", "pikachu-image");
-    pikachuContainer.appendChild(pikachuImage);
-
+    pikachuContainer.innerHTML = "";
     const pikachuName = document.createElement("h2");
-    pikachuName.setAttribute("id", "pikachu-name");
-    pikachuName.innerHTML = data.name;
     pikachuContainer.appendChild(pikachuName);
     pikachuContainer.appendChild(document.createElement("br"));
-    const pikachuStats = document.createElement("div");
+    const pikachuStats = document.createElement("div");``
     pikachuContainer.appendChild(document.createElement("br"));
     pikachuStats.setAttribute("id", "pikachu-stats");
     pikachuContainer.appendChild(pikachuStats);
     pikachuContainer.style.backgroundColor = "#D1D100";
+
+    //pikachu number
+    const pikachuNumber = document.createElement("h3");
+    pikachuNumber.setAttribute("id", "pikachu-number");
+    pikachuNumber.innerHTML = `${data.id} : pikachu`;
+    pikachuContainer.appendChild(pikachuNumber);
+    //pikachu image
+  
+    const pikachuImage = document.createElement("img");
+    pikachuImage.setAttribute("id", "pikachu-image");
+    pikachuImage.src = data.sprites.front_default;
+    pikachuContainer.appendChild(pikachuImage);
+
+    //pikachu type
+    const pikachuType = document.createElement("h3");
+    pikachuType.setAttribute("id", "pikachu-type");
+    pikachuType.innerHTML = `Type: ${data.types[0].type.name}`;
+    pikachuContainer.appendChild(pikachuType);
     
 
-    const pikachuStatNames = ["HP", "Attack", "Defense", "Special Attack", "Special Defense", "Speed"];
-    const pikachuStatValues = [
-      data.stats[0].base_stat,
-      data.stats[1].base_stat,
-      data.stats[2].base_stat,
-      data.stats[3].base_stat,
-      data.stats[4].base_stat,
-      data.stats[5].base_stat,
-    ];
+    // const pikachuStatNames = ["HP", "Attack", "Defense", "Special Attack", "Special Defense", "Speed"];
+    // const pikachuStatValues = [
+    //   data.stats[0].base_stat,
+    //   data.stats[1].base_stat,
+    //   data.stats[2].base_stat,
+    //   data.stats[3].base_stat,
+    //   data.stats[4].base_stat,
+    //   data.stats[5].base_stat,
+    // ];
 
-    for (let i = 0; i < pikachuStatNames.length; i++) {
-      const pikachuStat = document.createElement("p");
-      pikachuStat.setAttribute("id", "pikachu-stat")
-      pikachuStat.innerHTML = `${pikachuStatNames[i]}: ${pikachuStatValues[i]}`;
-      pikachuStats.appendChild(pikachuStat);
-    }
+    // for (let i = 0; i < pikachuStatNames.length; i++) {
+    //   const pikachuStat = document.createElement("p");
+    //   pikachuStat.setAttribute("id", "pikachu-stat")
+    //   pikachuStat.innerHTML = `${pikachuStatNames[i]}: ${pikachuStatValues[i]}`;
+    //   pikachuStats.appendChild(pikachuStat);
+    // }
   });
   })
   .catch(error => console.error(error));
